4571a5fa5820780ae64c21f809f8419a99b3bd4e
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-12-14 14:17:02 +0000

    reused methods for choosing best q
    reused methods for choosing best q

--- 1_data_fp_growth_logistic_regression.py
+++ 1_data_fp_growth_logistic_regression.py
@@ -21,6 +21,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+
 ######################################################################
 #Data
 itemsets = pd.read_pickle("./itemsets.pickle")
@@ -63,20 +66,23 @@ list_female_freq_items = list(df_female_fp["itemsets_strings"])
 
 ##################################################################
 #Subset features across all itemsets for Model
-df_ohe_female = df_ohe.loc[:, list_female_freq_items]
+df_ohe_female_frs = df_ohe.loc[:, list_female_freq_items]
 
 #Data
-X = df_ohe_female.drop(['Female'], axis=1)
+X = df_ohe_female_frs.drop(['Female'], axis=1)
 
 #Extract X, y
-y = df_ohe_female['Female']
+y = df_ohe_female_frs['Female']
 
 #Model set up
 #Data - Train + Test set
 testSizeX = 0.33 #67:33 split
 Xtrain, Xtest, ytrain, ytest, = train_test_split(X, y, test_size= testSizeX, random_state=42)
 #Count of female == 1
-np.count_nonzero(ytest)
+#np.count_nonzero(ytest)
+
+#************************************************************************************
+#Model 1 - Logistic Regression
 
 #Logistic Regression Classifier
 log_reg_model = LogisticRegression(penalty= 'l2')
@@ -91,4 +97,82 @@ predictions = log_reg_model.predict(Xtest)
 #Performance
 print(confusion_matrix(ytest, predictions))
 print(classification_report(ytest, predictions))
+
+#Choose best c
+#i. Choose c
+def choose_C_cv(X, y, c_range, plot_color):
+    '''Implement 5 fold cross validation for testing 
+    regression model (lasso or ridge) and plot results'''
+    
+    #Param setup
+    kf = KFold(n_splits = 5)
+    mean_error=[]; std_error=[];
+       
+    #Loop through each k fold
+    for c_param in c_range:
+        mse_temp = [] 
+        model = LogisticRegression(penalty= 'l2', C = c_param)
+                
+        for train_index, test_index in kf.split(X):           
+
+            model.fit(X.iloc[list(train_index)], y[train_index])
+            ypred = model.predict(X.iloc[list(test_index)])
+            mse = mean_squared_error(y[test_index],ypred)
+            mse_temp.append(mse)
+        
+        #Get mean & variance
+        mean_error.append(np.array(mse_temp).mean())
+        std_error.append(np.array(mse_temp).std())
+        
+    #Plot
+    plt.errorbar(c_range, mean_error, yerr=std_error, color = plot_color)
+    plt.xlabel('C')
+    plt.ylabel('Mean square error')
+    plt.title('Choice of C in Logistic regression - 5 fold CV')
+    plt.show()
+    
+#Implement
+c_range = [0.01, 0.02, 0.05, 1, 5, 10, 20, 50, 100, 500, 1000]
+plot_color = 'g'
+choose_C_cv(X, y, c_range, plot_color)
+
+
+
+#************************************************
+#knn 
+#i. Choose k
+def choose_k_knn(X, y, k_range, plot_color):
+    '''knn - Implement 5 fold cross validation for determinine optimal k'''
+    
+    #Param setup
+    kf = KFold(n_splits = 5)
+    mean_error=[]; std_error=[];
+       
+    #Loop through each k fold
+    for k in k_range:
+        mse_temp = []
+        model = KNeighborsClassifier(n_neighbors = k, weights= 'uniform')
+                
+        for train, test in kf.split(X):
+            
+            model.fit(X[train], y[train])
+            ypred = model.predict(X[test])
+            mse = mean_squared_error(y[test],ypred)
+            mse_temp.append(mse)
+        
+        #Get mean & variance
+        mean_error.append(np.array(mse_temp).mean())
+        std_error.append(np.array(mse_temp).std())
+        
+    #Plot
+    plt.errorbar(k_range, mean_error, yerr=std_error, color = plot_color)
+    plt.xlabel('k')
+    plt.ylabel('Mean square error')
+    plt.title('kNN - 5 fold CV')
+    plt.show()
+
+#Implement
+k_range = [2,3,5,7,8,10,15,20,25,40, 60, 100]
+plot_color = 'orange'
+choose_k_knn(X, y, k_range, plot_color)    
 
